13eff2c925cd1a139e6300776e8057f4ff124312
Fajarullah <frullah12@gmail.com>
2020-04-05 18:42:33 +0700

    root and about page

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class PageController < ApplicationController
+  def index; end
+
+  def about; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'page#index'
+
+  scope module: 'page' do
+    get :about
+  end
 end
